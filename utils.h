25a5cdbfa4892b334dffe24917ad2497eb845ff6
Katarzyna K <kasper.kasia@gmail.com>
2014-11-22 13:03:53 +0100

    one from few

--- utils.h
+++ utils.h
@@ -1,4 +1,53 @@
-#include <vector>
+include<vector>
+include<iostdlib>
+
+void show(std::vector<int> & tab){
+	std::vector<int> result = tab;
+	for(int i = 0; i < result.size(); i++){
+		std::cout << result[i] << std::endl;
+	}
+}
+
+
+
+int minFromVector(const std::vector<int> & tab)
+{
+	int min = tab[0];
+	for(int i = 0; i < tab.size(); ++i)
+	{
+		if(min > tab[i])
+		{
+			min = tab[i];
+		}
+	}
+	return min;
+}
+
+
+int maxFromVector(const std::vector<int> & tab)
+{
+	int max = tab[0];
+	for(int i = 0; i < tab.size(); ++i)
+	{
+		if(max < tab[i])
+		{
+			max = tab[i];
+		}
+	}
+	return max;
+}
+
+
+std::vector<int> generator()
+{
+	std::vector<int> tab(10);
+	for(int i = 0; i < 10; ++i){
+		tab.push_back(i);
+	}
+	return tab;
+}
+
+
 
 int sum(const std::vector<int>& tab)
 {
